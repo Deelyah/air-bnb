591aa37f7e1453eee5548119458f9a6e6f526942
PraiseDelia <elemdelia@gmail.com>
2023-07-07 09:18:41 +0100

    initial commit

---
+++ src/components/cards/ShowcaseCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="">
+      <img
+        src="https://a0.muscache.com/im/pictures/miso/Hosting-29589886/original/6250f7c4-0869-482e-b763-4f9d73046fb1.jpeg?im_w=720"
+        alt="place"
+        class="rounded-md"
+      />
+    </div>
+    <div class="flex w-full items-start">
+      <div>
+        <h3 class="font-bold text-[#222]">Teighntenmouth close</h3>
+        <p>Individual host</p>
+        <p>20-25 Nov</p>
+        <p class="font-bold text-[#222] underline">
+          $345 <span class="font-normal">total</span>
+        </p>
+      </div>
+      <div class="flex items-center ml-auto">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 32 32"
+          aria-hidden="true"
+          role="presentation"
+          focusable="false"
+          style="display: block; height: 12px; width: 12px; fill: currentcolor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="m15.1 1.58-4.13 8.88-9.86 1.27a1 1 0 0 0-.54 1.74l7.3 6.57-1.97 9.85a1 1 0 0 0 1.48 1.06l8.62-5 8.63 5a1 1 0 0 0 1.48-1.06l-1.97-9.85 7.3-6.57a1 1 0 0 0-.55-1.73l-9.86-1.28-4.12-8.88a1 1 0 0 0-1.82 0z"
+          ></path>
+        </svg>
+        <p class="ml-1">4.83</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
--- src/route.ts
+++ src/route.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "./pages/Home.vue";
+import ShowcaseCard from "./components/cards/ShowcaseCard.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: "/", component: Home }],
+  routes: [{ path: "/", component: ShowcaseCard }],
 });
 
 export default router;
